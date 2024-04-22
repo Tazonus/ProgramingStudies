3a3d03cca31658527ebb12cb10b094bb5099cc69
Taz <kontopgo1@gmail.com>
2024-04-22 17:00:34 +0200

    ADD list3.5 arguments from console

--- list3.py
+++ list3.py
@@ -1,15 +1,22 @@
 import re
+import sys
+
+#task 5
+
 brackets_open  = "{[(<"
 brackets_close = "}])>"
 
 #removing all symbols except brackets
-input = input("Write equation you want to check: \n")
+input = str(sys.argv)
 tempvector = []
 for x in input:
     if x in brackets_open:
         tempvector.append(x)
     elif x in brackets_close:
         id = brackets_close.index(x)
+        if len(tempvector) == 0:
+            print("expresion is invalid")
+            break
         if tempvector[-1] == brackets_open[id]:
             tempvector.pop()
         else:
@@ -17,3 +24,5 @@ for x in input:
             break
 if tempvector == []:
     print("expresion is valid")
+else:
+    print("expresion is invalid")
