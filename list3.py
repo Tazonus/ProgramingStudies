be9314ad2ed5b2a139af70c4ef448d174deedf47
Taz <kontopgo1@gmail.com>
2024-04-22 16:15:06 +0200

    FIX clearing temp_vector

--- list3.py
+++ list3.py
@@ -1,6 +1,6 @@
 import re
-brackets_open = "{[(<"
-brackets_close = ">)]}"
+brackets_open  = "{[(<"
+brackets_close = "}])>"
 
 #removing all symbols except brackets
 input = input("Write equation you want to check: \n")
@@ -9,9 +9,11 @@ for x in input:
     if x in brackets_open:
         tempvector.append(x)
     elif x in brackets_close:
-        if tempvector[-1] == x:
-            tempvector.pop
+        id = brackets_close.index(x)
+        if tempvector[-1] == brackets_open[id]:
+            tempvector.pop()
+        else:
+            print("expresion is invalid")#it was
+            break
 if tempvector == []:
     print("expresion is valid")
-else:
-    print('expresion is invalid')#or my code is bad
